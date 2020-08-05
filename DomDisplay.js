96ea3cef51e29c6aeedc87fcf7e049f58977a477
anshusaurav <anshu.saurav@gmail.com>
2020-08-05 15:01:26 +0530

    Improved Display Class

--- DomDisplay.js
+++ DomDisplay.js
@@ -1,16 +1,46 @@
 class DOMDisplay {
     constructor(parent, level) {
-        this.dom = elt("div", { class: "game" }, drawGrid(level));
+        this.dom = elt("div", { class: "game" }, this.drawGrid(level));
         this.actorLayer = null;
         parent.appendChild(this.dom);
     }
-
+    drawGrid(level) {
+        const scale = 20;
+        return elt(
+            "table",
+            {
+                class: "background",
+                style: `width: ${level.width * scale}px`,
+            },
+            ...level.rows.map((row) =>
+                elt(
+                    "tr",
+                    { style: `height: ${scale}px` },
+                    ...row.map((type) => elt("td", { class: type }))
+                )
+            )
+        );
+    }
+    drawActors(actors) {
+        return elt(
+            "div",
+            {},
+            ...actors.map((actor) => {
+                let rect = elt("div", { class: `actor ${actor.type}` });
+                rect.style.width = `${actor.size.x * scale}px`;
+                rect.style.height = `${actor.size.y * scale}px`;
+                rect.style.left = `${actor.pos.x * scale}px`;
+                rect.style.top = `${actor.pos.y * scale}px`;
+                return rect;
+            })
+        );
+    }
     clear() {
         this.dom.remove();
     }
     syncState(state) {
         if (this.actorLayer) this.actorLayer.remove();
-        this.actorLayer = drawActors(state.actors);
+        this.actorLayer = this.drawActors(state.actors);
         this.dom.appendChild(this.actorLayer);
         this.dom.className = `game ${state.status}`;
         this.scrollPlayerIntoView(state);
--- platformGame.js
+++ platformGame.js
@@ -1,16 +1,4 @@
 
-
-
-// Player.prototype.size = new Vector2D(0.8, 1.5);
-
-
-
-// Lava.prototype.size = new Vector2D(1, 1);
-
-
-
-// Coin.prototype.size = new Vector2D(0.6, 0.6);
-
 const levelChars = {
     " ": "empty",
     "x": "wall",
@@ -32,120 +20,45 @@ function elt(name, attrs, ...children) {
     }
     return dom;
 }
-
-// class DOMDisplay {
-//     constructor(parent, level) {
-//         this.dom = elt("div", { class: "game" }, drawGrid(level));
-//         this.actorLayer = null;
-//         parent.appendChild(this.dom);
-//     }
-
-//     clear() {
-//         this.dom.remove();
-//     }
-// }
-
 const scale = 20;
 
-function drawGrid(level) {
-    return elt(
-        "table",
-        {
-            class: "background",
-            style: `width: ${level.width * scale}px`,
-        },
-        ...level.rows.map((row) =>
-            elt(
-                "tr",
-                { style: `height: ${scale}px` },
-                ...row.map((type) => elt("td", { class: type }))
-            )
-        )
-    );
-}
+// function drawGrid(level) {
+//     return elt(
+//         "table",
+//         {
+//             class: "background",
+//             style: `width: ${level.width * scale}px`,
+//         },
+//         ...level.rows.map((row) =>
+//             elt(
+//                 "tr",
+//                 { style: `height: ${scale}px` },
+//                 ...row.map((type) => elt("td", { class: type }))
+//             )
+//         )
+//     );
+// }
 
-function drawActors(actors) {
-    return elt(
-        "div",
-        {},
-        ...actors.map((actor) => {
-            let rect = elt("div", { class: `actor ${actor.type}` });
-            rect.style.width = `${actor.size.x * scale}px`;
-            rect.style.height = `${actor.size.y * scale}px`;
-            rect.style.left = `${actor.pos.x * scale}px`;
-            rect.style.top = `${actor.pos.y * scale}px`;
-            return rect;
-        })
-    );
-}
+// function drawActors(actors) {
+//     return elt(
+//         "div",
+//         {},
+//         ...actors.map((actor) => {
+//             let rect = elt("div", { class: `actor ${actor.type}` });
+//             rect.style.width = `${actor.size.x * scale}px`;
+//             rect.style.height = `${actor.size.y * scale}px`;
+//             rect.style.left = `${actor.pos.x * scale}px`;
+//             rect.style.top = `${actor.pos.y * scale}px`;
+//             return rect;
+//         })
+//     );
+// }
 
-// DOMDisplay.prototype.syncState = function (state) {
-//     if (this.actorLayer) this.actorLayer.remove();
-//     this.actorLayer = drawActors(state.actors);
-//     this.dom.appendChild(this.actorLayer);
-//     this.dom.className = `game ${state.status}`;
-//     this.scrollPlayerIntoView(state);
-// };
-// DOMDisplay.prototype.scrollPlayerIntoView = function (state) {
-//     let width = this.dom.clientWidth;
-//     let height = this.dom.clientHeight;
-//     let margin = width / 3;
-
-//     // The viewport
-//     let left = this.dom.scrollLeft,
-//         right = left + width;
-//     let top = this.dom.scrollTop,
-//         bottom = top + height;
-
-//     let player = state.player;
-//     let center = player.pos.plus(player.size.times(0.5)).times(scale);
-
-//     if (center.x < left + margin) {
-//         this.dom.scrollLeft = center.x - margin;
-//     } else if (center.x > right - margin) {
-//         this.dom.scrollLeft = center.x + margin - width;
-//     }
-//     if (center.y < top + margin) {
-//         this.dom.scrollTop = center.y - margin;
-//     } else if (center.y > bottom - margin) {
-//         this.dom.scrollTop = center.y + margin - height;
-//     }
-// };
-// Level.prototype.touches = function (pos, size, type) {
-//     var xStart = Math.floor(pos.x);
-//     var xEnd = Math.ceil(pos.x + size.x);
-//     var yStart = Math.floor(pos.y);
-//     var yEnd = Math.ceil(pos.y + size.y);
-
-//     for (var y = yStart; y < yEnd; y++) {
-//         for (var x = xStart; x < xEnd; x++) {
-//             let isOutside = x < 0 || x >= this.width || y < 0 || y >= this.height;
-//             let here = isOutside ? "wall" : this.rows[y][x];
-//             if (here == type) return true;
-//         }
-//     }
-//     return false;
-// };
-
-// State.prototype.update = function (time, keys) {
-//     let actors = this.actors.map((actor) => actor.update(time, this, keys));
-//     let newState = new State(this.level, actors, this.status);
-
-//     if (newState.status != "playing") return newState;
-
-//     let player = newState.player;
-//     if (this.level.touches(player.pos, player.size, "lava")) {
-//         return new State(this.level, actors, "lost");
-//     }
-
-//     for (let actor of actors) {
-//         if (actor != player && overlap(actor, player)) {
-//             newState = actor.collide(newState);
-//         }
-//     }
-//     return newState;
-// };
-
+let playerSprites = document.createElement("img");
+playerSprites.src = "images/player.png";
+const playerXOverlap = 4;
+let otherSprites = document.createElement("img");
+otherSprites.src = "images/sprites.png";
 function overlap(actor1, actor2) {
     return (
         actor1.pos.x + actor1.size.x > actor2.pos.x &&
@@ -155,64 +68,10 @@ function overlap(actor1, actor2) {
     );
 }
 
-// Lava.prototype.collide = function (state) {
-//     return new State(state.level, state.actors, "lost");
-// };
-
-// Coin.prototype.collide = function (state) {
-//     let filtered = state.actors.filter((a) => a != this);
-//     let status = state.status;
-//     if (!filtered.some((a) => a.type == "coin")) status = "won";
-//     return new State(state.level, filtered, status);
-// };
-
-// Lava.prototype.update = function (time, state) {
-//     let newPos = this.pos.plus(this.speed.times(time));
-//     if (!state.level.touches(newPos, this.size, "wall")) {
-//         return new Lava(newPos, this.speed, this.reset);
-//     } else if (this.reset) {
-//         return new Lava(this.reset, this.speed, this.reset);
-//     } else {
-//         return new Lava(this.pos, this.speed.times(-1));
-//     }
-// };
 
 const wobbleSpeed = 8,
     wobbleDist = 0.07;
 
-// Coin.prototype.update = function (time) {
-//     let wobble = this.wobble + time * wobbleSpeed;
-//     let wobblePos = Math.sin(wobble) * wobbleDist;
-//     return new Coin(
-//         this.basePos.plus(new Vector2D(0, wobblePos)),
-//         this.basePos,
-//         wobble
-//     );
-// };
-
-
-
-// Player.prototype.update = function (time, state, keys) {
-//     let xSpeed = 0;
-//     if (keys.ArrowLeft) xSpeed -= playerXSpeed;
-//     if (keys.ArrowRight) xSpeed += playerXSpeed;
-//     let pos = this.pos;
-//     let movedX = pos.plus(new Vector2D(xSpeed * time, 0));
-//     if (!state.level.touches(movedX, this.size, "wall")) {
-//         pos = movedX;
-//     }
-
-//     let ySpeed = this.speed.y + time * gravity;
-//     let movedY = pos.plus(new Vector2D(0, ySpeed * time));
-//     if (!state.level.touches(movedY, this.size, "wall")) {
-//         pos = movedY;
-//     } else if (keys.ArrowUp && ySpeed > 0) {
-//         ySpeed = -jumpSpeed;
-//     } else {
-//         ySpeed = 0;
-//     }
-//     return new Player(pos, new Vector2D(xSpeed, ySpeed));
-// };
 
 function trackKeys(keys) {
     let down = Object.create(null);
@@ -271,4 +130,4 @@ async function runGame(plans, Display) {
     console.log("You've won!");
 }
 
-runGame(GAME_LEVELS, DOMDisplay);
+runGame(GAME_LEVELS, CanvasDisplay);
